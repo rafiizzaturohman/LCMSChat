935692e279dcb80bdcb14ba089775e1ff1fd55fe
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-05-04 09:32:24 +0700

    Initial Commit

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
-    
+    @Get()
+    fundAll() {
+        return []
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return { id }
+    }
 }
